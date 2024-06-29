5dec4de211c76aca0f15c96d9b054cd6082d4d18
kaitechvn <khaibui2604@gmail.com>
2024-06-29 17:47:14 +0700

    new

--- src/main/java/com/example/doan2/Doan2Application.java
+++ src/main/java/com/example/doan2/Doan2Application.java
@@ -18,19 +18,24 @@ public class Doan2Application {
 
 	@Bean
 	public CommandLineRunner commandLineRunner(ProductRepository productRepository){
+    String img1 = "assets/giaitich.jpg";
+    String img2 = "assets/daiso.jpg";
+
 		return args -> {
 			productRepository.save(Product.builder()
-					.id(UUID.randomUUID().toString())
-					.name("Giai tich")
+					.id("1")
+					.name("Giải tích ")
 					.price(2400)
 					.quantity(4)
+          .image(img1)
 					.build()
 			);
       productRepository.save(Product.builder()
-        .id(UUID.randomUUID().toString())
-        .name("Dai sp")
+        .id("2")
+        .name("Đại số")
         .price(3000)
         .quantity(5)
+        .image (img2)
         .build()
       );
 		};
--- src/main/java/com/example/doan2/entity/User.java
+++ src/main/java/com/example/doan2/entity/User.java
@@ -23,4 +23,6 @@ public class User {
   @NotEmpty
   private String password;
 
+
+
 }
